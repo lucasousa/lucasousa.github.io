fa02717d0bb7a37e64b3957142fe0cde64449bfd
root <root@LAPTOP-5J4P1IGG.localdomain>
2020-05-17 00:29:08 -0300

    Adicionando a parte dos serviços oferecidos e configurando layout desktop

--- static/javascript/funcoes.js
+++ static/javascript/funcoes.js
@@ -21,37 +21,3 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   }
 });
-
-// Smooth Anchor Scrolling
-$(document).on("click", 'a[href^="#"]', function(event) {
-  event.preventDefault();
-  $("html, body").animate(
-    {
-      scrollTop: $($.attr(this, "href")).offset().top
-    },
-    500
-  );
-});
-
-// When the user scrolls down 20px from the top of the document, show the scroll up button
-window.onscroll = function() {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("toTop").style.display = "block";
-  } else {
-    document.getElementById("toTop").style.display = "none";
-  }
-}
-
-// Preloader
-$(document).ready(function($) {
-  $(".preloader-wrapper").fadeOut();
-  $("body").removeClass("preloader-site");
-});
-$(window).on("load", function() {
-  var Body = $("body");
-  Body.addClass("preloader-site");
-});
